3926d0dce0c73f426b673fb0d6d2566be37a4468
Mero932 <mero93princessa@gmail.com>
2023-07-13 19:51:07 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-        
-    char Mystr[] = "Programming is like building a multilingual puzzle";
-
-    puts(Mystr);
+  
+    puts("Programming is like building a multilingual puzzle");
     return 0;
 }
 ~       
