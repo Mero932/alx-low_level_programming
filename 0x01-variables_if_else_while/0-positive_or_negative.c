ac88c50ed29a4ccb3a40ea7dd0192a007761ae02
Mero932 <mero93princessa@gmail.com>
2023-07-14 19:33:59 -0700

    low level programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,18 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
+unsigned int main(void)
 {
-	int n;
+	unsigned int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	{ printf("%u is positive\n", n);
-	}
+	{ printf("%u is positive", n);}
 	else if (n == 0)
-	{ printf("%is zero\n", n); 
-	}
+	{ printf("%is zero", n); }
 	else
-	{printf("%u, is negative\n", n); 
-	}
+	{printf("%u, is negative", n); }
 	return (0);
 }
