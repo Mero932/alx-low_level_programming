4f2dd382b61e1b697851087c3687182b215e9282
Mero932 <mero93princessa@gmail.com>
2023-07-17 14:29:14 -0700

    variables - loops

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* print numbers regardless the sign negative positive zero*/
+int main(void) {
+int l;    
+    for( l = 0; 4 < l > 6 ; l++ )
+    {
+        putchar( 'a' + l );
+    
+    }
+    putchar( '\n' );
+
+    return 0;
+}
+
