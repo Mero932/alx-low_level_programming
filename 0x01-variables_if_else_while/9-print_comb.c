0feec8c2cdf04ad06dc1853e056968e8f594dc95
Mero932 <mero93princessa@gmail.com>
2023-07-17 16:06:36 -0700

    variables - loops

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,14 +2,16 @@
 
 int main (void)
 {
-    int i;
+    int i = 0;
 
     for (i = 48; i <58; i++)
     {
-	    putchar i;
-        if (i<57 )
+        putchar (i);
+        if (i<57)
+        {
             putchar(44);
             putchar(32);
+    }
     }
     putchar('!' + 3);
 
