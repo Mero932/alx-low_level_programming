1cebdc947c50c1deb2e93b20e593db299c93bdcf
Mero932 <mero93princessa@gmail.com>
2023-07-17 10:21:43 -0700

    size of ..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 int32_t my_32bit_int;
-printf("size of a char: %lu \t byte(s)\n", sizeof(char));
-printf("size of an int: %lu\t byte(s)\n", sizeof(int));
-printf("size of a long int: %lu\t byte(s)\n", sizeof(long int));
-printf("size of a long long int: %lu\t byte(s)\n", sizeof(long long int));
-printf("size of a float: %lu\t byte(s)\n", sizeof(float));
+printf("size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
 
 int64_t my_64bit_int;
-printf("%lu\n", sizeof(char));
+printf("size of a char: %lu byte(s)\n", (2*sizeof(char)));
 printf("%lu\n", sizeof(int));
 printf("%lu\n", sizeof(float));
 printf("%lu\n", sizeof(double));
