a09185e232876dbd6d7b9df0c8d4875175c94add
Mero932 <mero93princessa@gmail.com>
2023-07-17 13:43:08 -0700

    variables - loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* print numbers regardless the sign negative positive zero*/
+int main(void) {
+int l;    
+    for( l = 0; l < 26; l++ )
+        putchar( 'a' + l );
+   
+for( l = 0; l < 26; l++ )
+        putchar( 'A' + l );
+    putchar( '\n' );
+
+    return 0;
+}
