fed4fbc6446c89c9e9cf247a90897e1b882811e5
Mero932 <mero93princessa@gmail.com>
2023-07-17 16:40:18 -0700

    variables - loops

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+/** 
+*main is providing the numbers from 1 to 9
+*/
 
 int main (void)
 {
-    int i = 0;
+int i = 0;
 
-    for (i = 48; i <58; i++)
-    {
-        putchar (i);
-        if (i<57)
-        {
-            putchar(44);
-            putchar(32);
-    }
-    }
-   putchar('\n'); 
-    return 0;
+for ( i = 48; i <  58; i++)
+{
+putchar (i);
+if (i < 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n'); 
+return 0;
 }
