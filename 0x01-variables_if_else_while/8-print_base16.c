9fe8eeba0a54bac25ba95f79c8201091ed640632
Mero932 <mero93princessa@gmail.com>
2023-07-17 15:15:27 -0700

    variables - loops

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int i = 0;
+
+    for (i = 0; i <= 9; i++)
+    {
+            putchar('0'+i);
+
+    }
+    
+    for (i = 0; i <= 5; i++)
+    {
+            putchar('a'+i);
+
+    }
+    putchar('\n');
+
+    return 0;
+}
