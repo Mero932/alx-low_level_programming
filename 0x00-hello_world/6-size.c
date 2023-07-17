c221d97a00999902812b86a505df2462a758d8d8
Mero932 <mero93princessa@gmail.com>
2023-07-17 10:51:36 -0700

    size of ..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,18 +6,22 @@
 */
 int main(void)
 {
-int32_t my_32bit_int;
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+int a;
+char b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %lu byte(s)\n", sizeof(b));
+printf("Size of int: %lu bytes\n", sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
 int64_t my_64bit_int;
-printf("Size of a char: %lu byte(s)\n", (2 * sizeof(char)));
-printf("Size of an int: %lu byte(s)\n", (2 * sizeof(int)));
-printf("Size of a long int: %lu byte(s)\n", (2 * sizeof(long int)));
-printf("Size of a long long int: %lu byte(s)\n", (2 * sizeof(long long int)));
-printf("Size of a float: %lu byte(s)\n", (2 * sizeof(float)));
+printf("Size of a char: %lu byte(s)\n", (2 * sizeof(b)));
+printf("Size of an int: %lu byte(s)\n", (2 * sizeof(a)));
+printf("Size of a long int: %lu byte(s)\n", (2 * sizeof(c)));
+printf("Size of a long long int: %lu byte(s)\n", (2 * sizeof(d)));
+printf("Size of a float: %lu byte(s)\n", (2 * sizeof(e)));
 return (0);
 }
