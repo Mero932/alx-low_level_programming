f46097754b4c65164e739b16cdff1f4ad49183f2
Mero932 <mero93princessa@gmail.com>
2023-07-17 18:16:56 -0700

    variables - loops

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 int l;
-for(l = 0; l < 26 ; l++)
+for (l = 0; l < 26 ; l++)
 if (l != 4 && l != 16)
-putchar ('a' + l );
+putchar ('a' + l);
 putchar ('\n');
 
 return (0);
