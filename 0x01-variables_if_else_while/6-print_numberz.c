a31c652e9b29c5b8991473f10a20d3c5114fc31a
Mero932 <mero93princessa@gmail.com>
2023-07-17 18:29:33 -0700

    variables - loops

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 
-int main (void)
+int main(void)
 {
-    int i = 0;
+int i = 0;
 
-    for (i = 0; i <= 9; i++) 
-    {
-            putchar('0'+i);
-    
-    }
-    putchar('\n');
+for (i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
 
-    return 0;
+return (0);
 }
