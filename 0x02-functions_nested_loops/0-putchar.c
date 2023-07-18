693940ad7259408a25c4e09ec2761589876288bb
Mero932 <mero93princessa@gmail.com>
2023-07-18 12:19:29 -0700

    0x02 function assignment

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* - Entry point
+* Return: Always 0 (Success)
+*/
+void printPutchar()
+{
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+}
+
+int main(void)
+{
+printPutchar();
+return (0);
+}
