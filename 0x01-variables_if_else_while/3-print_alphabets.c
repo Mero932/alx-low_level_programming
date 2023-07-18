97db3c21a719e67a0f1e379813b750070c252b51
Mero932 <mero93princessa@gmail.com>
2023-07-17 18:05:00 -0700

    variables - loops

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,11 @@
 int main(void)
 {
 int l;
-for(l = 0; l < 26; l++)
+for (l = 0; l < 26; l++)
 putchar( 'a' + l );
-   
-for(l = 0; l < 26; l++)
-putchar('A' + l);
+for (l = 0; l < 26; l++)
+putchar ('A' + l);
 putchar('\n');
 
-return( 0);
+return(0);
 }
