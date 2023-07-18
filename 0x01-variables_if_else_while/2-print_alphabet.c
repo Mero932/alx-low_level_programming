20fc06c101b6386035265a36c0ab89134f75ad02
Mero932 <mero93princessa@gmail.com>
2023-07-17 17:48:45 -0700

    variables - loops

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,11 @@
 * Return: Always 0 (Success)
 */
 
-int main( void )
+int main(void)
 {
-int l;   
-for( l = 0; l < 26; l++ )
-putchar( 'a' + l );
+int l;
+for(l = 0; l < 26; l++)
+putchar( 'a' + l);
 putchar('\n');
 
 return (0);
