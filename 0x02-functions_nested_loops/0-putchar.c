398fd4f046b7b68364d062c0f43a3bf1751487c7
Mero932 <mero93princessa@gmail.com>
2023-07-19 15:27:49 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
- /** 
-  * This function prints a character using the putchar function.
-  * Parameters:
-  * c (char): The character to be printed
-  * Returns:
-  * None
- */
+/**
+* This function prints a character using the putchar function.
+* Parameters:
+* c (char): The character to be printed
+* Returns:
+* None
+*/
 void print_putchar(char c)
 {
 putchar(c);
 }
-
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
 
-int main()
+int main(void)
 {
 print_putchar('_');
 print_putchar('p');
@@ -27,5 +26,5 @@ print_putchar('h');
 print_putchar('a');
 print_putchar('r');
 print_putchar('\n');
-return 0;
+return (0);
 }
