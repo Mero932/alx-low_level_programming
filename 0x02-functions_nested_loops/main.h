6e25d15fe1df60c715c208ebbb6ba77b1872cc39
Mero932 <mero93princessa@gmail.com>
2023-07-20 15:29:47 -0700

    function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,15 @@ void print_putchar(char c)
 putchar(c);
 }
 
+void print_alphabet(void) {
+    int i;
+
+    // Print the alphabet in lowercase
+    for (int i=97; i <= 122; i++)
+    {
+       _putchar(i);
+    }
+
+    _putchar(‘\n’);
+}
+
