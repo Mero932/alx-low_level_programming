906506e098456652a220c233bd9c7b6a9520e673
Mero932 <mero93princessa@gmail.com>
2023-07-20 06:56:00 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int _isupper(int c) {
+    /*
+    This function checks if a character is uppercase.
+    
+    Parameters:
+    c (char): The character to be checked
+    
+    Returns:
+    int: 1 if the character is uppercase, 0 otherwise
+    */
+    
+    if (c >= 'A' && c <= 'Z') {
+        return (1);
+    } else {
+        return (0);
+    }
+}
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void) {
+    int c;
+    
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    
+    if (_isupper(c)) {
+       // printf("The character is uppercase.\n");
+       printf("%c: %d\n", c, _isupper(c));
+    } else {
+        printf("The character is not uppercase.\n");
+    }
+    
+    return (0);
+}
