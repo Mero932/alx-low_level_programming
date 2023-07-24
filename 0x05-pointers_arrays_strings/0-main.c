4f26a738aa7a8c28e9000456f16ac16c993b5b4b
Mero932 <mero93princessa@gmail.com>
2023-07-24 12:11:59 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+int main (void)
+{
+     int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    /*putchar('n');
+    putchar('=');
+    putchar(48+n);*/
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    /*putchar('n');
+    putchar('=');
+    putchar(48+n);*/
+    return (0);
+}
