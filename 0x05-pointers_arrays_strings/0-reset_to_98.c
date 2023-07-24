340b5140e51dca79fb7eb59be623bd14de806299
Mero932 <mero93princessa@gmail.com>
2023-07-24 14:21:57 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - Entry
+* main - reset n to 98
+* n is the memory memort location
+* *n is the pointer
 * Return: Always 0 (Success)
 */
 
@@ -17,20 +19,20 @@ else
 }
 }
 
-/*int main (void)
-{
-     int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    putchar('n');
-    putchar('=');
-    putchar(48+n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    putchar('n');
-    putchar('=');
-    putchar(48+n);
-    return (0);
-  
-    }*/
+/*
+* int main (void)
+* {
+* int n;
+* n = 402;
+* printf("n=%d\n", n);
+* putchar('n');
+* putchar('=');
+* putchar(48+n);
+* reset_to_98(&n);
+* printf("n=%d\n", n);
+* putchar('n');
+* putchar('=');
+* putchar(48+n);
+* return (0);
+* }
+*/
