1c3bc79aa8d1012f78a4cc13c724c8e53c24faa2
Mero932 <mero93princessa@gmail.com>
2023-07-24 12:40:11 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-_putchar (int);
-void reset_to_98(int *n)
-{
-    if (*n != 98)
-    {*n = 98;
-    }
-    else
-    {*n = 98;}
-}
-
+int _putchar(char c);
+void reset_to_98(int *n);	
+#endif /* MAIN_H */
