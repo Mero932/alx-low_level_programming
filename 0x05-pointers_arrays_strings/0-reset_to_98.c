a92dce69fd914c2bf89cbae43ee320ce70cadb1f
Mero932 <mero93princessa@gmail.com>
2023-07-24 12:09:17 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <main.h>
+void reset_to_98(int *n)
+{
+    if (*n != 98)
+    {*n = 98;
+    }
+    else 
+    {*n = 98;}
+}
+
+int main (void)
+{
+     int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    /*putchar('n');
+    putchar('=');
+    putchar(48+n);*/
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    /*putchar('n');
+    putchar('=');
+    putchar(48+n);*/
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+_putchar (int);
+void reset_to_98(int *n)
+{
+    if (*n != 98)
+    {*n = 98;
+    }
+    else
+    {*n = 98;}
+}
+
