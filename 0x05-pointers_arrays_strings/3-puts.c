e7c68d81096c82abbb4c7ce1e512a85fc17766ae
Mero932 <mero93princessa@gmail.com>
2023-07-25 12:33:36 -0700

    pointers-arrays-strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,15 +3,14 @@
 /**
 * _puts - to print string
 * @s: pointer to display the character
-* @counter: integer parameter
 */
 
 void _puts(char *s)
 {
 int counter;
-for (counter=0; *s != '\0'; s++)
+for (counter = 0; *s != '\0'; s++)
 {
-counter ++;
+counter++;
 _putchar(*s);
 }
 _putchar('\n');
