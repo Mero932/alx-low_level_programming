160043c03b6c3adb9f38ca42960eccfb2e0dc7b1
Mero932 <mero93princessa@gmail.com>
2023-07-28 14:12:59 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,9 @@ void print_alphabet_x10(void)
 {
 int i;
 char a;
-for (i=0 ; i <= 9 ; i++)
+for (i = 0 ; i <= 9 ; i++)
 {
-for (a= 'a' ; a <= 'z' ; a++)
+for (a = 'a' ; a <= 'z' ; a++)
 {
 _putchar(a);
 }
