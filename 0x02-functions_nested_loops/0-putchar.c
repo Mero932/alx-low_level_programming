e90e624bbb6fde1f15a8339498677039b2051179
Mero932 <mero93princessa@gmail.com>
2023-07-28 02:56:03 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,11 @@
 /**
 * _putchar - This function prints a character using the putchar function.
 *
-* c : character parameter
+* @c : character parameter
+* Return: 1 (success)
+*        -1 (failure)
 */
 int _putchar(char c)
 {
-return (write (1, &c, 1));
+return (write(1, &c, 1));
 }
