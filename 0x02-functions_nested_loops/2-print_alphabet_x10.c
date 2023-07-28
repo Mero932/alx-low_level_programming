b6233303bf1a1eb96d3cb8fc4286fa3ad8536430
Mero932 <mero93princessa@gmail.com>
2023-07-28 13:03:27 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+/**
+* print_alphabet_x10 - lower case with a line between.
+*/
+void print_alphabet(void)
+{
+int i;
+char a;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (a = 'a' ; a <= 'z' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
