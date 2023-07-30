76fe0af44b37371bafa7a14119036ee8f069b583
Mero932 <mero93princessa@gmail.com>
2023-07-30 09:46:20 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  add - This function takes two integers as arguments and returns their sum.
+ *  @i: (int) The first integer to be added
+ *  @j: (int) The second integer to be added
+ *  Return: (i+j) integer The sum of the two integers
+ */
+int add(int i, int j)
+{
+return (i + j);
+}
