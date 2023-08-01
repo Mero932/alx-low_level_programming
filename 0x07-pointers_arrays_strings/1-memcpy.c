4869e66e925cbaa59b44f7c3e289b1e9514abf01
Mero932 <mero93princessa@gmail.com>
2023-07-31 19:28:02 -0700

    pointer arrays strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+* _memcpy - Entry point
+* @dest: char
+* @src: char
+* @n: unsigned int
+* Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int ptr;
+for (ptr = 0; n > 0 ; ptr ++, n--)
+{
+src[ptr] = dest[ptr];
+}
+return (dest);
+}
