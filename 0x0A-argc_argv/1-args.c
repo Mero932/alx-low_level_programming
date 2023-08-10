f12466b0e0eb03031cf35914201095ccc2995847
Mero932 <mero93princessa@gmail.com>
2023-08-09 19:49:30 -0700

    argc and agrv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the line
+ * @argc: int
+ * @argv: char
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%d\n", argc - 1);
+return (0);
+}
