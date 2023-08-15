9520a6e7b20eb1ba79701b43f74ac6c466cfae25
Mero932 <mero93princessa@gmail.com>
2023-08-14 20:03:31 -0700

    macros

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,4 @@
-/*
- * Header File: size_macro.h
- * This header file defines a macro named SIZE as an abbreviation for the token 1024.
-
-*/
- 
 #ifndef SIZE_MACRO_H
-#define SIZE_MACRO_H
- 
+#define SIZE_MACRO_H 
 #define SIZE 1024
- 
 #endif
