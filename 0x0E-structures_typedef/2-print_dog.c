7cc9c4aaca82b95f01f64b3e5c0121163d3ce01f
Mero932 <mero93princessa@gmail.com>
2023-08-15 19:55:07 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
  * print_dog - check the code
  * @d: struct
  */
- 
-void print_dog(struct dog *d) {
-if (d == NULL) {
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
 return;
 }
 printf("Name: %s\n", d->name ? d->name : "(nil)");
