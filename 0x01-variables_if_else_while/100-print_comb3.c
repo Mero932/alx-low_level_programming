2afe29dc7b575efc65a627f614513ee35b4996aa
Mero932 <mero93princessa@gmail.com>
2023-08-18 23:57:48 -0700

    100 numbers print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,25 +3,27 @@
 * main - Entry point
 * Return: Always 0 (Success)
 */
-int main ()
+int main(void)
 {
- int i,j,start,end;
- if (start < 0 || start > 9 || end < 0 || end > 9) 
- {
+int i, j, start, end;
+start = 0;
+end = 0;
+if (start < 0 || start > 9 || end < 0 || end > 9)
+{
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 10; j++) 
+for (j = 0; j < 10; j++)
 {
-if (i > j) 
+if (i > j)
 {
 continue;
 }
 putchar('0' + i);
 putchar('0' + j);
-putchar(' ');  
+putchar(' ');
 }
-putchar('\n');  
+putchar('\n');
 }
-} 
+}
 return (0);
 }
