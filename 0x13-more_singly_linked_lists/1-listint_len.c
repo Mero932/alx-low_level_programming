83f638d4baceff4a4560bfabbd4859d09eba37cd
Mareez <mero93princessa@gmail.com>
2023-08-29 19:52:23 -0700

    linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint - Print all the elements of a listint_t list
+ * @h: (const)
+ * Return: size_t The number of nodes in the linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
+~       
