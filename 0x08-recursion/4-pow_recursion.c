4ef50bb6743c2cb372738341153972471d120f6e
Mero932 <mero93princessa@gmail.com>
2023-10-01 12:31:26 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- *_pow_recursion - similar to man pow
+ * _pow_recursion - similar to man pow
  *
  * @x: The base number.
  * @y: The exponent.
+ * Return: (1) x raised to power of y, (-1) y is less than zero
  */
-int _pow_recursion(int x, int y) 
+int _pow_recursion(int x, int y)
 {
-if (y == 0) 
+if (y == 0)
 {
-return 1;
+return (1);
 }
-if (y < 0) 
+if (y < 0)
 {
-return -1;
+return (-1);
 }
-return x * _pow_recursion(x, y - 1);
+return (x * _pow_recursion(x, y - 1));
 }
