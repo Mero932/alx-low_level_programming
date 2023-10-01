9ded9e54bbedd4aff6666114c2fc274278b85532
Mero932 <mero93princessa@gmail.com>
2023-10-01 15:15:26 -0700

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,21 +2,32 @@
 /**
  * is_prime_number - Checks if a given integer is a prime number.
  *
- * @n: An integer to be checked.
- * return (1) input integer is a prime number, (0) otherwise.
+ * @n: The integer to be checked.
+ * Return: 1 integer is prime number, 0 otherwise.
  */
-int is_prime_number(int n) 
+int is_prime_number(int n)
 {
-if (n <= 1) 
-{
-return 0;
-}
-for (int i = 2; i * i <= n; i++) 
-{
-if (n % i == 0) 
+int i;
+if (n <= 1)
 {
 return (0);
 }
+if (n <= 3)
+{
+return (1);
+}
+if (n % 2 == 0 || n % 3 == 0)
+{
+return (0);
+}
+i = 5;
+if (i *i <= n)
+{
+if (n % i == 0 || n % (i + 2) == 0)
+{
+return (0);
+}
+i += 6;
 }
 return (1);
 }
