f57b469bb9f087ae33d9e99874e0b18f43177b2b
Mero932 <mero93princessa@gmail.com>
2023-10-02 14:13:28 -0700

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,20 +7,21 @@
 *Return: Always 0
 */
 int main(int argc, char *argv[])
-{   
+{
 int sum, num, j, i;
 sum = 0;
-if (argc == 1) 
+if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
-for (i = 1; i < argc; i++) {
+for (i = 1; i < argc; i++)
+{
 num = 0;
 j = 0;
-while (argv[i][j] != '\0') 
+while (argv[i][j] != '\0')
 {
-if (argv[i][j] < '0' || argv[i][j] > '9') 
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
 return (1);
