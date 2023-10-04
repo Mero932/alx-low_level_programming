b290e53336139711283a57000a0136a2e443438e
Mero932 <mero93princessa@gmail.com>
2023-10-04 15:19:49 -0700

    malloc free 2nd trial

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*free_grid - 2D grid
+*@s1: (cint) 
+*@s2: (cint) The second string
+*/
+void free_grid(int **grid, int height)
+{
+if (grid == NULL || height <= 0)
+{
+return;
+}
+for (int i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
+
