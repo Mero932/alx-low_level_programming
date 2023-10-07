dd1aea16ad124ca26fefbda24a9e287a1a7cd815
Mero932 <mero93princessa@gmail.com>
2023-10-07 12:34:57 -0700

    more free malloc 2nd trial

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+int _putchar(char c);
+int *array_range(int min, int max);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+#endif /* MAIN_H */
+
