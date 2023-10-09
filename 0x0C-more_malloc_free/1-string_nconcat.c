80f245c9058d28dfb81da1c6b0cb223adf325da3
Mero932 <mero93princessa@gmail.com>
2023-10-08 19:59:04 -0700

    more free malloc 2nd trial

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,22 +8,41 @@
  * @S1: char
  * @S2: char
  * @n: unsigned int
- * Return: Always 0.
+ * Return: (NULL) (new_str).
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len;	
-unsigned int len1;
-unsigned int len2;
-len1 = strlen(s1);
-len2 = strlen(s2);
-if (n >= len2)
+char *new_str;
+unsigned int s1_len, s2_len, i, j;
+if (s1 == NULL)
 {
-n = len2;
+s1 = "";
 }
-len = len1 + n;
-char *result = (char *)malloc((len + 1) * sizeof(char));
-strcpy(result, s1);
-strncat(result, s2, n);
-return (result);
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+;
+for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+;
+if (n >= s2_len)
+{
+n = s2_len;
+}
+new_str = malloc(sizeof(char) * (s1_len + n + 1));
+if (new_str == NULL)
+{
+return (NULL);
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+new_str[i] = s1[i];
+}
+for (j = 0; j < n; j++)
+{
+new_str[i + j] = s2[j];
+}
+new_str[i + j] = '\0';
+return (new_str);
 }
