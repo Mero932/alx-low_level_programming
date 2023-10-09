2bda9878c394df071c02d071a4db455afe6fd4b4
Mero932 <mero93princessa@gmail.com>
2023-10-08 20:08:12 -0700

    more free malloc 2nd trial

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,12 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr = malloc(nmemb *size);
+void *ptr = malloc(nmemb * size);
 if (ptr == NULL)
 {
 return (NULL);
 }
-memset(ptr, 0, nmemb *size);
+memset(ptr, 0, nmemb * size);
 return (ptr);
 }
 
