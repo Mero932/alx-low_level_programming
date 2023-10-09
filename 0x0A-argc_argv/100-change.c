2d1e5082c694e76217927e3cae017ecc54f88474
Mero932 <mero93princessa@gmail.com>
2023-10-09 13:52:01 -0700

    recursion 2nd trial

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,41 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Calculates the cents and coins
+ * @argc: int
+ * @argv: char*[]
+ * Return: (1) error, (0) less than zero,
+ */
 
 int main(int argc, char *argv[])
 {
 int cents, coins;
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
-
-    cents = atoi(argv[1]);
-
-    if (cents < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
-
-   coins = 0;
-
-    coins += cents / 25;
-    cents %= 25;
-
-    coins += cents / 10;
-    cents %= 10;
-
-    coins += cents / 5;
-    cents %= 5;
-
-    coins += cents / 2;
-    cents %= 2;
-
-    coins += cents;
-
-    printf("%d\n", coins);
-
-    return 0;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+coins = 0;
+coins += cents / 25;
+cents %= 25;
+coins += cents / 10;
+cents %= 10;
+coins += cents / 5;
+cents %= 5;
+coins += cents / 2;
+cents %= 2;
+coins += cents;
+printf("%d\n", coins);
+return (0);
 }
