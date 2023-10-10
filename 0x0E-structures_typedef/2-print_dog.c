5c4c5916bcf2129aea9417fe8c8b74823c8c770c
Mero932 <mero93princessa@gmail.com>
2023-10-10 14:30:03 -0700

    typedef 2nd trial

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,11 +7,10 @@
 
 void print_dog(struct dog *d)
 {
-if (d != NULL)
+if (d == NULL)
 {
 printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
 printf("owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
-
