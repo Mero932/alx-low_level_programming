b360d0ee8eb487d7eef1471a60c7fc88013961c6
Mero932 <mero93princessa@gmail.com>
2023-10-10 10:44:59 -0700

    preprocessor 2nd trial

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PRINT_FILE_NAME printf("%s\n", __FILE__)
+/**
+ * main - print file
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+PRINT_FILE_NAME;
+return (0);
+}
+
