a1f62457bc3b0a5ba5f33c8b35324c5a40c33e89
Mero932 <mero93princessa@gmail.com>
2023-10-10 13:54:28 -0700

    typedef 2nd trial

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * init_dog - dog details
+ * @d: char
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: Always 0.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-int i;	
-if (d == NULL || name == NULL || owner == NULL) {
+int i;
+if (d == NULL || name == NULL || owner == NULL)
+{
 printf("Invalid input parameters.\n");
 return;
 }
 d->name = (char *)malloc(sizeof(char));
-d->owner = (char *)malloc(sizeof(char)); 
+d->owner = (char *)malloc(sizeof(char));
 i = 0;
-while (name[i] != '\0') {
-        d->name[i] = name[i];
-        i++;
-    }
-d->name[i] = '\0'; 
+while (name[i] != '\0')
+{
+d->name[i] = name[i];
+i++;
+}
+d->name[i] = '\0';
 i = 0;
-while (owner[i] != '\0') {
+while (owner[i] != '\0')
+{
 d->owner[i] = owner[i];
 i++;
 }
-d->owner[i] = '\0'; 
+d->owner[i] = '\0';
 d->age = age;
 }
-
-
