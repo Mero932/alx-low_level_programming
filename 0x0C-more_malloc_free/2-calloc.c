6d552fa61b02f726ce574de659198f097c735362
Mero932 <mero93princessa@gmail.com>
2023-10-09 19:19:55 -0700

    more malloc free 2nd trial

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,22 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- * *_memset - return the ptr value
- * @s: char
- * @b: char
- * @n: unsigned int
- *
- * Return: ptr integer
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-char *ptr;
-*ptr = s;
-while (n--)
-*s++ = b;
-return (ptr);
-}
-/**
  * _calloc - prints buffer in hexa
  * @nmemb: the address of memory to print
  * @size: the size of the memory to print
@@ -27,13 +11,11 @@ return (ptr);
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *m;
-if (size == 0 || nmemb == 0)
+void *ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
 return (NULL);
-m = malloc(sizeof(int) * nmemb);
-if (m == 0)
-return (NULL);
-_memset(m, 0, sizeof(int) * nmemb);
-return (m);
 }
-
+memset(ptr, 0, nmemb * size);
+return (ptr);
+}
