0de94b2c0fd2eaefd91471e4e541fe84cc9c5754
Mero932 <mero93princessa@gmail.com>
2023-10-13 16:01:09 -0700

    vardiac functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,14 +1,51 @@
 #include <stdio.h>
-#include <stdarg.h>	 
-/**
- * @print_all - Print anything using a format string.
- * @format: int format The format string.
-*/
+#include <stdarg.h>
+
 void print_all(const char * const format, ...)
 {
-va_list args;
-va_start(args, format);
-vprintf(format, args);
-va_end(args);
+    va_list args;
+    char *s;
+    int i;
+    float f;
+    char c;
+    int j = 0;
+
+    va_start(args, format);
+
+    while (format && format[j])
+    {
+        switch (format[j])
+        {
+            case 'c':
+                c = va_arg(args, int);
+                printf("%c", c);
+                break;
+            case 'i':
+                i = va_arg(args, int);
+                printf("%d", i);
+                break;
+            case 'f':
+                f = va_arg(args, double);
+                printf("%f", f);
+                break;
+            case 's':
+                s = va_arg(args, char *);
+                if (s == NULL)
+                {
+                    printf("(nil)");
+                    break;
+                }
+                printf("%s", s);
+                break;
+            default:
+                j++;
+                continue;
+        }
+        if (format[j + 1])
+            printf(", ");
+        j++;
+    }
+    printf("\n");
+    va_end(args);
 }
 
