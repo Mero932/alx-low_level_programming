87102bd2a36abb00858191384f4ecd8465fddc62
Mero932 <mero93princessa@gmail.com>
2023-10-15 14:12:31 -0700

    recursion 2nd trial

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,12 @@
 int sqrt_helper(int n, int start, int end);
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-return (-1);
-}
-return (sqrt_helper(n, 0, n));
+   if (n < 0)
+        return -1;
+    if (n == 0 || n == 1)
+        return n;
+
+    return _sqrt_helper(n, 1, n);
 }
 /**
  * sqrt_helper - sqrt helper
@@ -27,22 +28,19 @@ int sqrt_helper(int n, int start, int end)
 {
 int mid;
 int square;
-if (start > end)
-{
-return (-1);
-}
-mid = (start + end) / 2;
-square = mid *mid;
-if (square == n)
-{
-return (mid);
-}
-else if (square < n)
-{
-return (sqrt_helper(n, mid + 1, end));
-}
-else
-{
-return (sqrt_helper(n, start, mid - 1));
-}
+   mid = (start + end) / 2;
+
+    if (mid * mid == n)
+        return mid;
+    if (mid * mid < n)
+    {
+        if ((mid + 1) * (mid + 1) > n)
+            return mid;
+        else
+            return _sqrt_helper(n, mid + 1, end);
+    }
+    else
+    {
+        return _sqrt_helper(n, start, mid - 1);
+    }
 }
