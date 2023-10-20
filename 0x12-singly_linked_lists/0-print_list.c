fe43ba0c6a71872e49823648f3c98644ced7c03d
Mero932 <mero93princessa@gmail.com>
2023-10-19 19:41:10 -0700

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,18 @@
 /**
- * print_list - prints all the elements
+ * print_list - Prints all the elements of a linked list.
  *
- * @h: int
- * Return: count
+ * @h: int  A pointer to the head of the linked list.
+ * Return: The number of nodes in the linked list
  */
-size_t print_list(const list_t *h)
+
+size_t print_list(const list_t *h);
 {
 size_t count = 0;
-const list_t *current = h;
-while (current != NULL)
+while (h)
 {
-if (current->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%d] %s\n", current->len, current->str);
-}
+printf("[%d] %s\n ", strlen(h->str), h->str ? h->str : "nil");
+h =  h->next;
 count++;
-current = current->next;
 }
 return (count);
 }
