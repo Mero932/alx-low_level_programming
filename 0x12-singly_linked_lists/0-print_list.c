ebf24d80108a04f8a27a1aec9c9d68ef94a6282c
Mero932 <mero93princessa@gmail.com>
2023-10-20 15:21:31 -0700

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,12 @@
  */
 int _strlen(const char* str) 
 {
-    int length = 0;
- 
-      while (str[length] != '\0') {
-        length++;
-    }
- 
-    return length;
+int length = 0;
+if(!s)
+return(0);
+while (*ss)[
+length ++;
+return (length);
 }
 
 /**
