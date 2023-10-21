7ab9b38fb54ca9ea8cb35e37db6a2e33ef546390
Mero932 <mero93princessa@gmail.com>
2023-10-21 08:34:25 -0700

    recursion 2nd trial

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,33 @@
 #include "main.h"
 /**
- * _sqrt_recursion - Calculates the square root of a number
- *
- * @n: int for which the square root needs to be calculated.
- * @square: int
- * Return - sqrt helper n
- */
-int sqrt_helper(int n, int square);
-int _sqrt_recursion(int n)
-{
-return (sqrt_helper(n, 1));
-}
-/**
  *sqrt_helper - sqrt helper
  *@n: int
- *@squre: int
+ *@square: int
  *
  *Return: square, -1
  */
 int sqrt_helper(int n, int square)
 {
 if (square * square == n)
+{
 return (square);
+}
 if (square * square < n)
 {
 return (sqrt_helper(n, square + 1));
 }
 else
+{
 return (-1);
+}
+}
+/**
+ * _sqrt_recursion - Calculates the square root of a number
+ *
+ * @n: int for which the square root needs to be calculated.
+ * Return: n integer, 1
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt_helper(n, 1));
 }
