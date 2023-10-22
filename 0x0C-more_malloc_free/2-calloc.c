868b6f5e77f5fb11398f1d39c4d3eeced36781a0
Mero932 <mero93princessa@gmail.com>
2023-10-22 10:16:07 -0700

    more malloc 2nd trial

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,10 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr = malloc(nmemb * size);
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 if (ptr == NULL)
 {
 return (NULL);
