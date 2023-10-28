6457eba0e9764a669c46a5d4cf846150053566a0
Mero932 <mero93princessa@gmail.com>
2023-10-27 19:59:26 -0700

    bit manipulated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,11 +4,31 @@
  * print_binary - change binary  to unsigned int
  * @n: unsigned long int
  */
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
-if (n > 1) 
+unsigned long int highest_bit;
+highest_bit = 1;
+if (n == 0)
 {
-print_binary(n >> 1);
+printf("0");
+return;
 }
-printf("%lu", n & 1);
+while (highest_bit <= n)
+{
+highest_bit <<= 1;
 }
+highest_bit >>= 1;
+while (highest_bit > 0)
+{
+if (n & highest_bit)
+{
+printf("1");
+}
+else
+{
+printf("0");
+}
+highest_bit >>= 1;
+}
+}
+
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,13 +5,14 @@
  * @index: unsigned int
  * Return: -1, 1
  */
-int set_bit(unsigned long int *n, unsigned int index) 
+int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int mask;
 if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-unsigned long int mask = 1UL << index;
+mask = 1UL << index;
 *n |= mask;
 return (1);
 }
