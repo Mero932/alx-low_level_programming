ee34586402492b557f8550594df5f582a128800a
Mero932 <mero93princessa@gmail.com>
2023-10-27 20:04:39 -0700

    bit manipulated

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,12 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask;
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index > (sizeof(unsigned long int) * 8) - 1)
 return (-1);
-}
-mask = 1UL << index;
-*n |= mask;
+*n |= (1UL << index);
 return (1);
 }
