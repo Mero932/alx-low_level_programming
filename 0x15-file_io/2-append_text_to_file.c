6118edb550bbddc7b33764f796c1942fa0bbd177
Mero932 <mero93princessa@gmail.com>
2023-11-03 15:18:46 -0700

    File descriptors and permissions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,32 +3,22 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
- * create_file - creates a file with the given filename and writes the
- * given text content to it
- * @filename: the name of the file to create
- * @text_content: the text content to write to the file
+ * append_text_to_file - Appends text at the end of a file.
  *
- * Return: 1 on success, -1 on failure
+ * @filename: const char.
+ * @text_content: char
+ * Return: 1 
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content) 
 {
-int fd, len = 0;
-if (filename == NULL)
-return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (fd == -1)
-return (-1);
-if (text_content != NULL)
+FILE *file;
+file = fopen(filename, "a");
+if (file == NULL) 
 {
-while (text_content[len])
-len++;
-if (write(fd, text_content, len) != len)
-{
-close(fd);
-return (-1);
+printf("Failed to open the file.\n");
+return (0);
 }
-}
-close(fd);
+fprintf(file, "%s", text_content);
+fclose(file);
 return (1);
 }
-
