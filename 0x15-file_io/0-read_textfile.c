0961e6ab1993a8bd5462cc8949157aed634f15aa
Mero932 <mero93princessa@gmail.com>
2023-11-03 14:55:05 -0700

    File descriptors and permissions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,22 +11,24 @@
 ssize_t read_textfile(const char *filename, size_t letters) 
 {
 char *buffer;
+FILE *file;
+ssize_t bytesRead;
 if (filename == NULL) 
 {
 return (0);
 }
-FILE *file = fopen(filename, "r");
+file = fopen(filename, "r");
 if (file == NULL) 
 {
 return (0);
 }
-*buffer = malloc(sizeof(char) * (letters + 1));
+buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL) 
 {
 fclose(file);
 return (0);
 }
-ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
+bytesRead = fread(buffer, sizeof(char), letters, file);
 if (bytesRead <= 0) 
 {
 fclose(file);
