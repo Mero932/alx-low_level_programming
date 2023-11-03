515c8f58e7bfdeecb762f3d2d382faedc18ffe2e
Mero932 <mero93princessa@gmail.com>
2023-11-03 14:59:01 -0700

    File descriptors and permissions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 /**
- * ssize_t read_textfile - read and print 
+ * read_textfile - read and print
  * @filename: const char
  * @letters: size_t
  *
@@ -8,28 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters) 
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buffer;
 FILE *file;
 ssize_t bytesRead;
-if (filename == NULL) 
+if (filename == NULL)
 {
 return (0);
 }
 file = fopen(filename, "r");
-if (file == NULL) 
+if (file == NULL)
 {
 return (0);
 }
 buffer = malloc(sizeof(char) * (letters + 1));
-if (buffer == NULL) 
+if (buffer == NULL)
 {
 fclose(file);
 return (0);
 }
 bytesRead = fread(buffer, sizeof(char), letters, file);
-if (bytesRead <= 0) 
+if (bytesRead <= 0)
 {
 fclose(file);
 free(buffer);
