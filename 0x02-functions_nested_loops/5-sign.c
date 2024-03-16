48861568977666407a5f80860ba90a449e6bd962
Mero932 <mero93princessa@gmail.com>
2024-03-16 08:03:08 -0700

    function nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - prints the sign of a number.
+* @n: integer
+* Return: (1) greater than zero , (0) zero , (-1) less than zero
+*/
+int print_sign(int n) 
+{
+if (n > 0) 
+{
+printf("+\n");
+return (1);
+} 
+else if (n == 0) 
+{
+printf("0\n");
+return (0);
+} 
+else 
+{
+printf("-\n");
+return (-1);
+}
+}
+
