6475ac69d620ef09a4722182ade359e7f8a9c5c5
Mero932 <mero93princessa@gmail.com>
2024-03-24 15:26:10 -0700

    functions nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,19 +6,24 @@
  */
 void times_table(void)
 {
-int i;
+int i, result;
 i = 0;
 while (i <= 9)
 {
-_putchar('0' + i);
-_putchar(' ');
-_putchar('x');
-_putchar(' ');
-_putchar('9');
-_putchar(' ');
-_putchar('=');
-_putchar(' ');
-_putchar('0' + i*9);
+result = i * 9;
+/*
+*_putchar('0' + i);
+*_putchar(' ');
+*_putchar('x');
+*_putchar(' ');
+*_putchar('9');
+*_putchar(' ');
+*_putchar('=');
+*_putchar(' ');
+*_putchar('0' + i * 9);
+*/
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
 _putchar('\n');
 i++;
 }
